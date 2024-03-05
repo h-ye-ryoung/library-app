886178991d8a1233f015b50ca66d8917cb341857
hyeryoung <pot46@naver.com>
2024-03-06 01:46:42 +0900

    Controller 3단 분리(1)

--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -1,4 +1,15 @@
 package com.group.libraryapp.repository.user;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class UserRepository {
+    public boolean isUserNotExist(JdbcTemplate jdbcTemplate, long id) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    public void updateUserName(JdbcTemplate jdbcTemplate, String name, long id) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
 }
--- src/main/java/com/group/libraryapp/service/user/UserService.java
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -1,19 +1,19 @@
 package com.group.libraryapp.service.user;
 
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.repository.user.UserRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserService {
 
+    private final UserRepository userRepository = new UserRepository();
     public void updateUser(JdbcTemplate jdbcTemplate, UserUpdateRequest request) {
-        String readSql = "SELECT * FROM user WHERE id = ?";
-        boolean isUserNotExist
-                = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        if (isUserNotExist) {
+        if (userRepository.isUserNotExist(jdbcTemplate, request.getId())) {
             throw new IllegalArgumentException();
         }
 
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userRepository.updateUserName(jdbcTemplate, request.getName(), request.getId());
     }
+
+
 }
